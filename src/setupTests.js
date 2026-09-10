7cde6a450c2b0fa93e18f8b24ee9db565b734ba2
Ammisetty Chamundeswari <131233976+Ammisettychamu33@users.noreply.github.com>
2026-09-10 21:04:37 +0530

    Enhance ResizeObserver polyfill to immediately supply non-zero dimensions to ResponsiveContainer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,14 +1,37 @@
 import '@testing-library/jest-dom'
 
 class DummyResizeObserver {
-  observe() {}
+  constructor(callback) {
+    this.callback = callback
+  }
+
+  observe(element) {
+    if (typeof this.callback === 'function') {
+      this.callback([
+        {
+          target: element,
+          contentRect: {
+            width: 500,
+            height: 500,
+            top: 0,
+            left: 0,
+            bottom: 500,
+            right: 500,
+          },
+          borderBoxSize: [{ inlineSize: 500, blockSize: 500 }],
+        },
+      ])
+    }
+  }
+
   unobserve() {}
+
   disconnect() {}
 }
 
-if (typeof window !== 'undefined' && !window.ResizeObserver) {
+if (typeof window !== 'undefined') {
   window.ResizeObserver = DummyResizeObserver
 }
-if (typeof global !== 'undefined' && !global.ResizeObserver) {
+if (typeof global !== 'undefined') {
   global.ResizeObserver = DummyResizeObserver
 }
