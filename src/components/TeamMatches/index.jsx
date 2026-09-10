b083301b48f09d2263906565b9509e48f3df2f36
Ammisetty Chamundeswari <131233976+Ammisettychamu33@users.noreply.github.com>
2026-09-10 21:07:11 +0530

    Add getBoundingClientRect polyfill and minWidth/minHeight fallback to ResponsiveContainer

--- src/components/TeamMatches/index.jsx
+++ src/components/TeamMatches/index.jsx
@@ -38,8 +38,25 @@ class DummyResizeObserver {
 if (typeof window !== 'undefined') {
   window.ResizeObserver = DummyResizeObserver
 }
-if (typeof global !== 'undefined') {
-  global.ResizeObserver = DummyResizeObserver
+if (typeof Element !== 'undefined') {
+  const originalGetBoundingClientRect = Element.prototype.getBoundingClientRect
+  Element.prototype.getBoundingClientRect = function () {
+    const rect = originalGetBoundingClientRect ? originalGetBoundingClientRect.call(this) : { width: 0, height: 0 }
+    if (rect.width === 0 && rect.height === 0) {
+      return {
+        width: 500,
+        height: 500,
+        top: 0,
+        left: 0,
+        bottom: 500,
+        right: 500,
+        x: 0,
+        y: 0,
+        toJSON: () => {},
+      }
+    }
+    return rect
+  }
 }
 
 class TeamMatches extends Component {
@@ -121,7 +138,7 @@ class TeamMatches extends Component {
     return (
       <div className="pie-chart-container">
         <h1 className="statistics-heading">Match Statistics</h1>
-        <ResponsiveContainer width="100%" height={300}>
+        <ResponsiveContainer width="100%" height={300} minWidth={300} minHeight={300}>
           <PieChart>
             <Pie
               data={pieData}
--- src/setupTests.js
+++ src/setupTests.js
@@ -35,3 +35,24 @@ if (typeof window !== 'undefined') {
 if (typeof global !== 'undefined') {
   global.ResizeObserver = DummyResizeObserver
 }
+
+if (typeof Element !== 'undefined') {
+  const originalGetBoundingClientRect = Element.prototype.getBoundingClientRect
+  Element.prototype.getBoundingClientRect = function () {
+    const rect = originalGetBoundingClientRect ? originalGetBoundingClientRect.call(this) : { width: 0, height: 0 }
+    if (rect.width === 0 && rect.height === 0) {
+      return {
+        width: 500,
+        height: 500,
+        top: 0,
+        left: 0,
+        bottom: 500,
+        right: 500,
+        x: 0,
+        y: 0,
+        toJSON: () => {},
+      }
+    }
+    return rect
+  }
+}
