ac871844a4aeebfdabc60f494f3aca7304b16237
Ammisetty Chamundeswari <131233976+Ammisettychamu33@users.noreply.github.com>
2026-09-10 21:08:59 +0530

    Remove ResponsiveContainer to enable synchronous PieChart & Legend rendering, add nameKey to Pie and name to Cell

--- src/components/TeamMatches/index.jsx
+++ src/components/TeamMatches/index.jsx
@@ -138,25 +138,24 @@ class TeamMatches extends Component {
     return (
       <div className="pie-chart-container">
         <h1 className="statistics-heading">Match Statistics</h1>
-        <ResponsiveContainer width="100%" height={300} minWidth={300} minHeight={300}>
-          <PieChart>
-            <Pie
-              data={pieData}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              dataKey="value"
-              isAnimationActive={false}
-              label
-            >
-              {pieData.map((entry, index) => (
-                <Cell key={`cell-${entry.name}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-            <Tooltip />
-            <Legend verticalAlign="bottom" height={36} />
-          </PieChart>
-        </ResponsiveContainer>
+        <PieChart width={320} height={300}>
+          <Pie
+            data={pieData}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            dataKey="value"
+            nameKey="name"
+            isAnimationActive={false}
+            label
+          >
+            {pieData.map((entry, index) => (
+              <Cell key={`cell-${entry.name}`} fill={COLORS[index % COLORS.length]} name={entry.name} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend verticalAlign="bottom" height={36} />
+        </PieChart>
       </div>
     )
   }
